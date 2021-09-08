b6b86bd797ebf335df09cc66c6713d519d93c038
Gabriel Pires Antunes <gabriellpantunes@gmail.com>
2021-09-07 23:17:12 -0300

    chg: Removido busca de casos e adicionado jobrunr suporte

---
+++ src/main/java/br/com/gabriellpa/joborchestrator/v1/client/CasesApiClient.java
@@ -0,0 +1,13 @@
+package br.com.gabriellpa.joborchestrator.v1.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "cases-api", url = "${api.cases.url}")
+public interface CasesApiClient {
+
+    @GetMapping
+    void getData(@RequestHeader("Cases_Type") String casesType);
+
+}
